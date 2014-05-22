08964ea47b9b153826fdeb43c825f2f6c134df05
David <david.romero@trescloud.com>
2014-05-22 13:07:59 -0500

    TO DO - issue FDU-335: Bloqueo en pagos en exceso https://trescloud.atlassian.net/browse/FDU-335 Creacion del modulo mas campo para validar cuando se debe o no permitir este pago mayor al importe

--- ecua_payment/__openerp__.py
+++ ecua_payment/__openerp__.py
@@ -5,12 +5,13 @@
         "website" : "http://www.trescloud.com",
         "category" : "Vertical Modules/Parametrization",
         "description": """Payment """,
-        "depends" : ["base"],
+        "depends" : ["base","account_voucher"],
         "init_xml" : [ ],
         #"data" : [ 'data/ir.model.access.csv',],
         "demo_xml" : [ ],
-        "update_xml" : ['security/account_security.xml',
-                        'security/ir.model.access.csv',
+        "update_xml" : [
+                        'views/account_voucher_view.xml',
+                        #'security/ir.model.access.csv',
                         ],
         "installable": True
      
---
+++ ecua_payment/objects/account_voucher.py
@@ -0,0 +1,33 @@
+import time
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+import openerp.addons.decimal_precision as dp
+
+class account_voucher(osv.osv):
+    _inherit = 'account.voucher'
+    _name = 'account.voucher'
+
+
+    _columns = {
+        'luck': fields.boolean('Luck amount'),
+                }
+    _defaults = {  
+        'luck': True
+        }
+         
+    def proforma_voucher(self, cr, uid, ids, context=None):
+        "Si luck es True para bloquear y manar la ecepcion caso contrario que no mande la ecepcion y "
+        obj_voucher_self = self.browse(cr, uid, ids)
+        obj_voucher_line = self.pool.get('account.voucher.line')
+        for o in obj_voucher_self:
+            if o.luck:
+                for so_ids in obj_voucher_line.search(cr,uid,[('voucher_id','=',o.id)]):
+                    line = obj_voucher_line.browse(cr, uid, so_ids)
+                    "comparar para lanzar excepcion"
+                    if line.amount > line.amount_unreconciled: 
+                        raise osv.except_osv(_('Warning!'),
+                                        _('Importe de la linea a Pagar debe ser menor o igual al Saldo inicial de la linea!'))                    
+            else:
+                return super(account_voucher, self).proforma_voucher(cr, uid, id, default, context)
+                
+account_voucher()
