c15589f9cca723b213ec582b80c3493c3f727e2d
pepetreshere <andres.calle@trescloud.com>
2014-11-10 17:42:43 -0500

    QA de contribuyente especial

--- l10n_ec_niif_minimal/objects/res_company.py
+++ l10n_ec_niif_minimal/objects/res_company.py
@@ -8,6 +8,6 @@ from openerp.tools.translate import _
 class res_company(osv.Model):
     _inherit = 'res.company'
     _columns = {
-        'contri_especial':fields.char('Special contributor', required=False, help='Resolution Special contributor'),
+        'special_tax_contributor_number':fields.char('Special Contributor Number', required=False, help='Special Contributors are designed by the Ecuadorian Tax Authority, the number is used in tax reporting'),
     }
 res_company()        
