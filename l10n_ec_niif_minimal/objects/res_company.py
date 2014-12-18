52667df893cb6dc0d540babef493324ccf741b66
luismasuelli-trescloud <luis.masuelli@trescloud.com>
2014-12-18 16:13:29 -0500

    FDU-651 Puesto el campo de "Obligado a llevar contabilidad" en el modelo de Compañia

--- l10n_ec_niif_minimal/objects/res_company.py
+++ l10n_ec_niif_minimal/objects/res_company.py
@@ -10,5 +10,9 @@ class res_company(osv.Model):
     _columns = {
         'special_tax_contributor_number':fields.char('Special Contributor Number', required=False, help='Special Contributors are designed by the Ecuadorian Tax Authority, the number is used in tax reporting'),
         'comercial_name':fields.related('partner_id','comercial_name',type='char',relation='res.partner',string='Comercial Name',store=True),
+        'forced_accounting': fields.boolean('Forced to maintain accounting books')
+    }
+    _defaults = {
+        'forced_accounting': False
     }
 res_company()        
