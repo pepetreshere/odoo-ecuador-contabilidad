15bb970c493c00255fe0eeaa3cd3cca8b919db17
luismasuelli-trescloud <luis.masuelli@trescloud.com>
2015-01-07 13:06:00 -0500

    FDU-680 ponemos los campos y su validacion

--- ecua_invoice/objects/shop.py
+++ ecua_invoice/objects/shop.py
@@ -29,6 +29,7 @@ from osv import fields, osv
 from tools import config
 from tools.translate import _
 
+
 class sale_shop(osv.osv):
     _inherit = 'sale.shop'
     _columns = {
@@ -68,15 +69,79 @@ class sale_shop(osv.osv):
     
 sale_shop()
 
-    
+
 class sri_printer_point(osv.osv):
     
     _name = 'sri.printer.point'
     
     _columns = {
-                'name':fields.char('Name', size=3, required=True,help='This number is assigned by the SRI'), 
-                'shop_id':fields.many2one('sale.shop', 'Shop'),
-                }
+                'name': fields.char('Name', size=3, required=True,help='This number is assigned by the SRI'),
+                'shop_id': fields.many2one('sale.shop', 'Shop'),
+                'invoice_sequence': fields.many2one('ir.sequence', string='Customer Invoices sequential', required=False,
+                                                    help='If specified, will be used by the printer point to specify '
+                                                         'the next number for the invoices'),
+                'refund_sequence': fields.many2one('ir.sequence', string='Customer Refunds sequential', required=False,
+                                                   help='If specified, will be used by the printer point to specify '
+                                                        'the next number for the credit notes'),
+                'debit_note_sequence': fields.many2one('ir.sequence', string='Debit Notes sequential', required=False,
+                                                       help='If specified, will be used by the printer point to specify'
+                                                            ' the next number for the debit notes'),
+                'withhold_sequence': fields.many2one('ir.sequence', string='Withholds sequential', required=False,
+                                                     help='If specified, will be used by the printer point to specify '
+                                                          'the next number for the withholds'),
+                'waybill_sequence': fields.many2one('ir.sequence', string='Waybills sequential', required=False,
+                                                    help='If specified, will be used by the printer point to specify '
+                                                         'the next number for the waybills'),
+    }
+
+    def _verify_repeated_sequences(self, cr, uid, ids, context=None):
+        """
+        This constraint checks whether the specified sequences are in use by other printer points.
+
+        The logic comes as follows:
+            * We collect the assigned sequences, and check whether values are not repeated.
+              * It is an error to have repeated values among the sequence fields in the same object.
+            * We check that the specified sequence values are not used in any other object. For
+              that, we check for each object whether any of its sequence fields has a sequence IN the
+              generated list from the current sequence, and has a distinct id (i.e. we're not including
+              the current instance in the same criteria).
+              * It is an error to find another object which shares either of the sequences in either of
+                the fields.
+        """
+
+        for instance in self.browse(cr, uid, ids, context=None):
+            values = [p and p.id for p in [
+                instance.invoice_sequence,
+                instance.refund_sequence,
+                instance.debit_note_sequence,
+                instance.withhold_sequence,
+                instance.waybill_sequence
+            ] if p]
+
+            if values:
+                #We evaluate this because we set at least one of
+                #such fields to an existent ir.sequence reference.
+
+                if len(values) != len(set(values)):
+                    #If the length of the list is not the same as the length of a set
+                    #with the same elements, it means that the list has REPEATED elements.
+                    #
+                    #We must get pissed off if that's the case.
+                    return False
+                found = self.search(cr, uid, ['&', ('id', '!=', instance.id),
+                                              '|', ('invoice_sequence', 'in', values),
+                                              '|', ('refund_sequence', 'in', values),
+                                              '|', ('debit_note_sequence', 'in', values),
+                                              '|', ('withhold_sequence', 'in', values),
+                                                   ('waybill_sequence', 'in', values)], context=None)
+                if found:
+                    #We found an ID which is distinct to the current instance's ID AND
+                    #also it has at least one sequence field with value among the values of
+                    #the same fields in the current instance.
+                    #
+                    #We must get pissed off if that's the case.
+                    return False
+        return True
     
     def name_get(self,cr,uid,ids, context=None):
         if not context:
@@ -100,6 +165,12 @@ class sri_printer_point(osv.osv):
             if obj.invoice_sequence_id:
                 obj.invoice_sequence_id.unlink()
         return super(sri_printer_point, self).unlink(cr, uid, ids, context=context)
+
+    _constraints = (
+        (_verify_repeated_sequences,
+         'Error: Al menos uno de los secuenciales está repetido o en uso dentro de otro Punto de Impresión',
+         ['invoice_sequence', 'refund_sequence', 'debit_note_sequence', 'withhold_sequence', 'waybill_sequence']),
+    )
     
 sri_printer_point()
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
