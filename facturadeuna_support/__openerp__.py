17e3ce9119a952e510bcdeb29cc9b5838e3f759c
luismasuelli-trescloud <luis.masuelli@trescloud.com>
2015-01-13 10:56:07 -0500

    FDU-655 FDU-654: Menu de nuevas funcionalidades

---
+++ facturadeuna_support/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'FacturaDeUna Support',
+    'author': 'Trescloud S.A.',
+    'category': 'Support',
+    "version": "1.0",
+    "website": "http://www.trescloud.com/",
+    "description": """Provide support links for Factura De Una""",
+    "depends": ["mail", "web"],
+    "data": [],
+    "demo_xml": [],
+    "init_xml": [],
+    "update_xml": ['menu.xml'],
+    "installable": True,
+    'js': ['static/src/js/facturadeuna_support.js'],
+    'css': ['static/src/css/facturadeuna_support.css'],
+    'qweb': ['static/src/qweb/facturadeuna_support.xml'],
+}
+
