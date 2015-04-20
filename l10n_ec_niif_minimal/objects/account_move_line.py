4083d0eb3a873137389900b8ff4ce3004ad2c82f
luismasuelli-trescloud <luis.masuelli@trescloud.com>
2015-04-20 09:51:37 -0500

    FDU-818 Sacada temporalmente la funcionalidad

--- l10n_ec_niif_minimal/objects/account_move_line.py
+++ l10n_ec_niif_minimal/objects/account_move_line.py
@@ -29,70 +29,70 @@ class account_move_line(osv.osv):
     _inherit = "account.move.line"
     _name = 'account.move.line'
 
-    def name_get(self, cr, uid, ids, context=None):
-        """
-        Genera un nombre basado en 3 cosas: nombre del movimiento, nombre de la factura asociada, nombre de asiento.
-        """
-        def _name(obj):
-            # referenciamos invoice.name
-            return '%s (%s)%s' % (
-                obj.move_id.name or '',
-                obj.ref,
-                obj.invoice and obj.invoice.name and (' (Inv: %s)' % (obj.invoice.name,)) or ''
-            )
-        return [(obj.id, _name(obj)) for obj in self.browse(cr, uid, ids, context)]
+    # def name_get(self, cr, uid, ids, context=None):
+    #     """
+    #     Genera un nombre basado en 3 cosas: nombre del movimiento, nombre de la factura asociada, nombre de asiento.
+    #     """
+    #     def _name(obj):
+    #         # referenciamos invoice.name
+    #         return '%s (%s)%s' % (
+    #             obj.move_id.name or '',
+    #             obj.ref,
+    #             obj.invoice and obj.invoice.name and (' (Inv: %s)' % (obj.invoice.name,)) or ''
+    #         )
+    #     return [(obj.id, _name(obj)) for obj in self.browse(cr, uid, ids, context)]
 
-    def name_search(self, cr, uid, name='', args=None, operator='ilike', context=None, limit=100):
-        """
-        Buscamos el nombre con el operador elegido, en los campos de nombre de factura, nombre de movimiento, y
-          referencia del movimiento. Para esto, juntamos los criterios normales del search (los que recibimos como
-          `args` y les concatenamos la siguiente lógica:
-
-          Rompemos el nombre buscado en todas sus "palabras", separando por espacios, y contemplamos la posibilidad
-            de que cada "palabra" aparezca en cualquiera de los tres campos: invoice.name, ref, y move_id.name.
-        """
-        name = str(name).strip()
-        if name:
-            parts = str(name).split()
-            args = list(args or ())
-            parts_args = []
-            for part in parts:
-                """
-                La idea de este bucle es que juntamos todos los criterios (serian como sub-dominios) que nos
-                  armamos con cada "palabra" (tras haber roto el nombre en "palabras").
-
-                Entonces por un lado concatenamos todos los posibles dominios, nos quedaría algo así como:
-
-                para "123 hola": ['|', '|', ('invoice.name', operator, '123'),
-                                            ('move_id.name', operator, '123'),
-                                            ('ref', operator, '123'),
-                                  '|', '|', ('invoice.name', operator, 'hola'),
-                                            ('move_id.name', operator, 'hola'),
-                                            ('ref', operator, 'hola')]
-
-                Además de concatenar así estos criterios, del otro lado (por la izquierda del array) ponemos
-                  operadores '|', uno por cada "palabra". Al finalizar el bucle, tenemos que POPear el primer
-                  elemento del array (que será el de más a la izquierda: un '|') ya que si hay N terminos que
-                  ponemos, deben haber N-1 operadores '|' entre ellos.
-
-                Finalmente: tenemos la garantía de que a este bucle se ingresará al menos una vez, ya que al
-                  comenzar este checkeo hicimos un .strip() y preguntamos si el nombre tenía "algo". Quiere
-                  decir que tras invocarle .split() tendremos al menos un elemento para iterar, por lo que
-                  al menos un '|' sobrante existirá.
-                """
-                parts_args.extend(['|', '|',
-                                  ('invoice.name', operator, part),
-                                  ('move_id.name', operator, part),
-                                  ('ref', operator, part)])
-                parts_args.insert(0, '|')
-            parts_args.pop(0)
-            # Y ahora componemos de la siguiente manera: [a1, a2, ..., an, pa1, pa2, ..., pan]
-            #   Ya que se exigirá que el search subyacente cumpla los criterios de args originales
-            #   Y (&) los criterios de parts_args posteriores.
-            #
-            # Para componernos esto nos basta simplemente con sumar ambas listas.
-            ids = self.search(cr, uid, args + parts_args, offset=0, limit=limit, context=context)
-        else:
-            ids = []
-        return self.name_get(cr, uid, ids, context=context)
+    # def name_search(self, cr, uid, name='', args=None, operator='ilike', context=None, limit=100):
+    #     """
+    #     Buscamos el nombre con el operador elegido, en los campos de nombre de factura, nombre de movimiento, y
+    #       referencia del movimiento. Para esto, juntamos los criterios normales del search (los que recibimos como
+    #       `args` y les concatenamos la siguiente lógica:
+    #
+    #       Rompemos el nombre buscado en todas sus "palabras", separando por espacios, y contemplamos la posibilidad
+    #         de que cada "palabra" aparezca en cualquiera de los tres campos: invoice.name, ref, y move_id.name.
+    #     """
+    #     name = str(name).strip()
+    #     if name:
+    #         parts = str(name).split()
+    #         args = list(args or ())
+    #         parts_args = []
+    #         for part in parts:
+    #             """
+    #             La idea de este bucle es que juntamos todos los criterios (serian como sub-dominios) que nos
+    #               armamos con cada "palabra" (tras haber roto el nombre en "palabras").
+    #
+    #             Entonces por un lado concatenamos todos los posibles dominios, nos quedaría algo así como:
+    #
+    #             para "123 hola": ['|', '|', ('invoice.name', operator, '123'),
+    #                                         ('move_id.name', operator, '123'),
+    #                                         ('ref', operator, '123'),
+    #                               '|', '|', ('invoice.name', operator, 'hola'),
+    #                                         ('move_id.name', operator, 'hola'),
+    #                                         ('ref', operator, 'hola')]
+    #
+    #             Además de concatenar así estos criterios, del otro lado (por la izquierda del array) ponemos
+    #               operadores '|', uno por cada "palabra". Al finalizar el bucle, tenemos que POPear el primer
+    #               elemento del array (que será el de más a la izquierda: un '|') ya que si hay N terminos que
+    #               ponemos, deben haber N-1 operadores '|' entre ellos.
+    #
+    #             Finalmente: tenemos la garantía de que a este bucle se ingresará al menos una vez, ya que al
+    #               comenzar este checkeo hicimos un .strip() y preguntamos si el nombre tenía "algo". Quiere
+    #               decir que tras invocarle .split() tendremos al menos un elemento para iterar, por lo que
+    #               al menos un '|' sobrante existirá.
+    #             """
+    #             parts_args.extend(['|', '|',
+    #                               ('invoice.name', operator, part),
+    #                               ('move_id.name', operator, part),
+    #                               ('ref', operator, part)])
+    #             parts_args.insert(0, '|')
+    #         parts_args.pop(0)
+    #         # Y ahora componemos de la siguiente manera: [a1, a2, ..., an, pa1, pa2, ..., pan]
+    #         #   Ya que se exigirá que el search subyacente cumpla los criterios de args originales
+    #         #   Y (&) los criterios de parts_args posteriores.
+    #         #
+    #         # Para componernos esto nos basta simplemente con sumar ambas listas.
+    #         ids = self.search(cr, uid, args + parts_args, offset=0, limit=limit, context=context)
+    #     else:
+    #         ids = []
+    #     return self.name_get(cr, uid, ids, context=context)
 account_move_line()
