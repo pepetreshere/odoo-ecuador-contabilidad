a8a993b46b57bf00f86f6369568bc1d669487c99
mrpavy <mrpavy@gmail.com>
2015-05-19 10:12:42 -0500

    Modulo de impuestos 2015

---
+++ ecua_2015_tax/__openerp__.py
@@ -0,0 +1,14 @@
+{
+        "name" : "ecua_2015_tax",
+        "version" : "1",
+        "author" : "TRESCLOUD CIA. LTDA- Ing. Pablo Vizhnay",
+        "website" : "http://www.trescloud.com",
+        "category" : "Vertical Modules/Parametrization",
+        "description": """Modulo contiene todas las actualizaciones de impuestos 2015""",
+        "depends" : ["account","base"],
+        "init_xml" : [ ],
+        "demo_xml" : [ ],
+        'data': ['cuentas_2015.xml','codigos_impuestos_2015.xml','impuestos_2015.xml'],
+
+        "installable": True
+} 
